f7af649793785e89384cd275e59495463398c0ca
imhireki <hireki@proton.me>
2024-02-14 17:00:03 -0300

    [chore]: remove deprecated ColorCluster

--- color/cluster.py
+++ color/cluster.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
-from typing import Iterator
 
-from PIL.Image import Image
 
 if TYPE_CHECKING:
     from color.palette import ColorBands, ColorIterator, Color, IColor
@@ -10,39 +8,6 @@ if TYPE_CHECKING:
 
 SplitRGB = list[list[int]]
 RGB = tuple[int, ...]
-
-
-class ColorCluster:
-    def __init__(self, image: Image) -> None:
-        self._image: Image = image
-
-    def get_colors(self) -> list[RGB]:
-        rgb_sequences = self._get_rgb_sequences()
-        colors = self._make_colors(rgb_sequences)
-        colors_with_incidences = self._calculate_color_incidences(colors)
-        return self._sort_colors_by_incidences(colors_with_incidences)
-
-    def _get_rgb_sequences(self) -> SplitRGB:
-        return [self._image.getdata(band) for band in range(3)]
-
-    def _make_colors(self, rgb_sequences: SplitRGB) -> Iterator[RGB]:
-        return map(lambda *rgb: tuple(rgb), *rgb_sequences)
-
-    def _calculate_color_incidences(self, colors: Iterator[RGB]) -> dict[RGB, int]:
-        color_with_incidences: dict[RGB, int] = {}
-
-        for color in colors:
-            color_with_incidences[color] = color_with_incidences.get(color, 0) + 1
-        return color_with_incidences
-
-    def _sort_colors_by_incidences(
-        self, colors_with_incidences: dict[RGB, int]
-    ) -> list[RGB]:
-        return sorted(
-            colors_with_incidences,
-            key=lambda color_with_incidences: color_with_incidences[1],
-            reverse=True,
-        )
 
 
 class SortedColorCluster:
--- tests/test_color/test_cluster.py
+++ tests/test_color/test_cluster.py
@@ -1,21 +1,6 @@
 from color import cluster
 
 
-def test_color_cluster(mocker):
-    expected_sorted_colors = [(255, 255, 255), (0, 0, 0)]
-    vertically_aligned_rgb = [
-        (255, 255, 255, 0, 0),  # Red
-        (255, 255, 255, 0, 0),  # Green
-        (255, 255, 255, 0, 0),  # Blue
-    ]
-    image = mocker.Mock(getdata=lambda band: vertically_aligned_rgb[band])
-
-    color_cluster = cluster.ColorCluster(image)
-    colors = color_cluster.get_colors()
-
-    assert colors == expected_sorted_colors
-
-
 def test_sorted_color_cluster(mocker):
     color = mocker.Mock(
         get_color_bands=lambda: [],
