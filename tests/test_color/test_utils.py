ca086d956c95e61ffae367c9a0a1b86a59a9d04c
imhireki <hireki@proton.me>
2025-02-14 19:23:12 -0300

    [test]: replace test_rgb_to_hex with test_rgb_or_rgba_to_hex

--- tests/test_color/test_utils.py
+++ tests/test_color/test_utils.py
@@ -1,6 +1,6 @@
 from color import utils
 
 
-def test_rgb_to_hex():
-    assert utils.rgb_to_hex((255, 255, 255)) == "#ffffff"
-    assert utils.rgb_to_hex((66, 135, 245)) == "#4287f5"
+def test_rgb_or_rgba_to_hex():
+    assert utils.rgb_or_rgba_to_hex((66, 135, 245)) == "#4287f5"
+    assert utils.rgb_or_rgba_to_hex((4, 1, 255, 100)) == "#0401ff64"
