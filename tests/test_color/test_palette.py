5cd22263e437ff8fe504a81bed06d422ecf1bad0
imhireki <hireki@proton.me>
2025-02-15 12:49:13 -0300

    [test]: prepare IColor-related tests to handle alpha as an argument

--- tests/test_color/test_palette.py
+++ tests/test_color/test_palette.py
@@ -1,30 +1,36 @@
+import pytest
+
 from color import palette
 
 
+@pytest.mark.parametrize("alpha", [True, False])
 class TestHexRGB:
-    def test_get_color_bands(self, mocker, rgba_bands):
+    def test_get_color_bands(self, mocker, rgba_bands, alpha):
         image = mocker.Mock(getdata=lambda band: rgba_bands[band])
-        color = palette.HexRGB(image)
-        assert color.get_color_bands() == rgba_bands[:3]
+        color = palette.HexRGB(image, alpha)
+        assert color.get_color_bands() == rgba_bands[: 3 + 1 if alpha else 3]
 
-    def test_structure_raw_palette(self, mocker, rgba_bands):
-        rgb_to_hex = mocker.patch("color.utils.rgb_to_hex", return_value="#ffffff")
-        color = palette.HexRGB(mocker.Mock())
+    def test_structure_raw_palette(self, mocker, rgba_bands, alpha):
+        rgb_to_hex = mocker.patch(
+            "color.utils.rgb_or_rgba_to_hex", return_value="#ffffff"
+        )
+        color = palette.HexRGB(mocker.Mock(), alpha)
 
-        rp = list(color.structure_raw_palette(rgba_bands[:3]))
+        rp = list(color.structure_raw_palette(rgba_bands[: 3 + 1 if alpha else 3]))
 
-        rgb_to_hex.assert_any_call((1, 1, 1))
-        rgb_to_hex.assert_any_call((2, 2, 2))
+        rgb_to_hex.assert_any_call((1, 1, 1, 1) if alpha else (1, 1, 1))
+        rgb_to_hex.assert_any_call((2, 2, 2, 2) if alpha else (2, 2, 2))
         assert rp == ["#ffffff"] * 2
 
 
-class TestRGBA:
-    def test_get_color_bands(self, mocker, rgba_bands):
+@pytest.mark.parametrize("alpha", [True, False])
+class TestRGB:
+    def test_get_color_bands(self, mocker, rgba_bands, alpha):
         image = mocker.Mock(getdata=lambda band: rgba_bands[band])
-        color = palette.RGBA(image)
-        assert color.get_color_bands() == rgba_bands
+        color = palette.RGB(image, alpha)
+        assert color.get_color_bands() == rgba_bands[: 3 + 1 if alpha else 3]
 
-    def test_structure_raw_palette(self, mocker, rgba_bands):
-        color = palette.RGBA(mocker.Mock())
-        rp = list(color.structure_raw_palette(rgba_bands))
-        assert rp == [(1, 1, 1, 1), (2, 2, 2, 2)]
+    def test_structure_raw_palette(self, mocker, rgba_bands, alpha):
+        color = palette.RGB(mocker.Mock(), alpha)
+        rp = list(color.structure_raw_palette(rgba_bands[: 3 + 1 if alpha else 3]))
+        assert rp == [(1, 1, 1, 1), (2, 2, 2, 2)] if alpha else [(1, 1, 1), (2, 2, 2)]
